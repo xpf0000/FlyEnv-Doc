220c3feb3304458b931d5a3308bc9945c52454fc
徐鹏飞 <250881478@qq.com>
2026-02-03 19:11:40 +0800

    upload version

--- docs/components/AppDown/index.vue
+++ docs/components/AppDown/index.vue
@@ -395,19 +395,19 @@
   const { lang } = useData()
 
   const urls = {
-    win: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-Setup-4.13.0.exe',
+    win: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-Setup-4.12.2.exe',
     winPortable:
-      'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-Portable-4.13.0.exe',
-    macX86: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0.dmg',
-    macArm: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0-arm64.dmg',
+      'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-Portable-4.12.2.exe',
+    macX86: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2.dmg',
+    macArm: 'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2-arm64.dmg',
     linuxDebX64:
-      'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0-x64.deb',
+      'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2-x64.deb',
     linuxDebArm64:
-      'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0-arm64.deb',
+      'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2-arm64.deb',
     linuxRpmX64:
-      'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0-x64.rpm',
+      'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2-x64.rpm',
     linuxRpmArm64:
-      'https://github.com/xpf0000/FlyEnv/releases/download/v4.13.0/FlyEnv-4.13.0-arm64.rpm'
+      'https://github.com/xpf0000/FlyEnv/releases/download/v4.12.2/FlyEnv-4.12.2-arm64.rpm'
   }
 
   const title = computed(() => {
